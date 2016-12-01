3200e4ba7dd368b4097b9bf0ddd240c215bbb46f
Mohammad Hussin Saad <moha2896@edu.easj.dk>
2016-12-01 10:38:13 +0100

    Første design

---
+++ App11/Model/FællesViewmodel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Navigation;
+
+namespace App11.Model
+{
+    class FællesViewmodel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public FællesViewmodel()
+        {
+
+        }
+
+        private void GotoMainpage2_Button(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+} 
+
+
+
+    /*protected virtual void onPropertyChanged (string propertyName)
+           {
+               if(PropertyChanged != null)
+               {
+                   PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+               }
+
+      */
+
