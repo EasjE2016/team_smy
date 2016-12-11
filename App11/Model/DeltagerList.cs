658985da333a3d9c45b8e2c3ba40d2a88187e0f8
Mohammad Hussin Saad <moha2896@edu.easj.dk>
2016-12-11 23:43:53 +0100

    Mad og arbejde liste

--- App11/Model/DeltagerList.cs
+++ App11/Model/DeltagerList.cs
@@ -14,28 +14,7 @@ namespace App11.Model
         { 
 
      
-        /*   // :base()hh
         
-            DeltagerList mandagsListe = new DeltagerList();
-            mandagsListe.Add(new Deltagere() { husNr = 1, antalVoksne = 2, antalUnge = 1, antalSmåBørn = 0, antalStoreBørn = 1 });
-            mandagsListe.Add(new Deltagere() { husNr = 2, antalVoksne = 1, antalUnge = 2, antalSmåBørn = 1, antalStoreBørn = 0 });
-            mandagsListe.Add(new Deltagere() { husNr = 3, antalVoksne = 2, antalUnge = 0 });
-
-            DeltagerList tirsdagsListe = new DeltagerList();
-            tirsdagsListe.Add(new Deltagere() { husNr = 1, antalVoksne = 2, antalUnge = 1, antalSmåBørn = 0, antalStoreBørn = 1 });
-            tirsdagsListe.Add(new Deltagere() { husNr = 2, antalVoksne = 1, antalUnge = 2, antalSmåBørn = 1, antalStoreBørn = 0 });
-            tirsdagsListe.Add(new Deltagere() { husNr = 3, antalVoksne = 2, antalUnge = 0 });
-
-            DeltagerList onsdagsListe = new DeltagerList();
-            onsdagsListe.Add(new Deltagere() { husNr = 1, antalVoksne = 2, antalUnge = 1, antalSmåBørn = 0, antalStoreBørn = 1 });
-            onsdagsListe.Add(new Deltagere() { husNr = 2, antalVoksne = 1, antalUnge = 2, antalSmåBørn = 1, antalStoreBørn = 0 });
-            onsdagsListe.Add(new Deltagere() { husNr = 3, antalVoksne = 2, antalUnge = 0 });
-
-            DeltagerList torsdagsListe = new DeltagerList();
-            torsdagsListe.Add(new Deltagere() { husNr = 1, antalVoksne = 2, antalUnge = 1, antalSmåBørn = 0, antalStoreBørn = 1 });
-            torsdagsListe.Add(new Deltagere() { husNr = 2, antalVoksne = 1, antalUnge = 2, antalSmåBørn = 1, antalStoreBørn = 0 });
-            torsdagsListe.Add(new Deltagere() { husNr = 3, antalVoksne = 2, antalUnge = 0 });
-*/
         }
     }
 }
